3c532b1d7cbd08b8d16d50664a3b06d67b980a3b
Ivonne <ivvyami@outlook.com>
2019-09-22 13:28:40 -0500

    got it to filter data and show filtered data on table

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -11,26 +11,55 @@ button.on("click", function() {
 
     // Get the value property of the input element
 
-    var inputValue = inputElement.property("value"); 
+    var inputValue = input.property("value"); 
 
     console.log(inputValue); 
     console.log(tableData);
 
-    var filteredData = tableData.filter(tbl => tbl.datetime === inputValue);
+    // making variable for filtered data based on the inputValue entered
 
+    var filteredData = tableData.filter(tbl => tbl.datetime === inputValue);
+    
+    // console log the filtered data in the to see what we got!
     console.log(filteredData);
 
+    // these are the filtered values that we want in the table
     var dateTime = filteredData.map(tbl => tbl.datetime);
     var city = filteredData.map(tbl => tbl.city);
     var state = filteredData.map(tbl => tbl.state);
     var country = filteredData.map(tbl => tbl.country);
     var shape = filteredData.map(tbl => tbl.shape);
     var comment = filteredData.map(tbl => tbl.comments);
+    
 
+    // select the table body, you'll be using this later
     var tbody = d3.select("tbody");
 
-    tableData.forEach(function(ufoSightings) {
-        console.log(ufoSightings);
+    filteredData.forEach(ufoSighting => {
+        var row = tbody.append("tr");
+        Object.entries(ufoSighting).forEach(([key, value]) => {
+            var cell = row.append("td");
+            cell.text(value);
+        });
     });
+       
+    
+    // // loop through the filtered data and console.log all of it
+    // tableData.forEach(function(filteredData) {
+    //     console.log(filteredData);
+
+    //     //append one table row 'tr' for each ufo sighting
+    //     var row = tbody.append("tr");
+
+    //     // console log each ufo sighting value
+    //     Object.entries(filteredData).forEach(function([key,value]) {
+    //         console.log(key, value);
+    //         //use d3 to append 1 cell per ufo sighting value (datetime, city, state
+    //         // country, shape, comment)
+    //         var cell = row.append("td");
+    //         cell.text(value);
+    //     });
+
+    // });
 });
 
